bfdfd8bd9b42a5cd9797bdadebd7739b2886c022
harshshah456 <harshshahkhs456@gmail.com>
2017-02-07 21:36:29 -0800

    Fragment commit

---
+++ app/src/main/java/com/janvi/hp/myfragmentdemo/One.java
@@ -0,0 +1,20 @@
+package com.janvi.hp.myfragmentdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by HP on 2/1/2017.
+ */
+public class One extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.one,container,false);
+        return view;
+    }
+}
